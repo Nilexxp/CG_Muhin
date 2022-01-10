3934d771aa56cada7e07811fd47e19ef73ce0804
Николай Пепеляев <nilexxp@gmail.com>
2022-01-10 22:00:28 +0500

    Reworked Spline function, final colours

--- GraphEditor/WindowSpline.xaml.cs
+++ GraphEditor/WindowSpline.xaml.cs
@@ -1,17 +1,10 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace GraphEditor
@@ -19,40 +12,43 @@ namespace GraphEditor
     /// <summary>
     /// Логика взаимодействия для WindowSpline.xaml
     /// </summary>
+    
     public partial class WindowSpline : Window
     {
+        const int NumOfPoints = 10;
+
+
         Random random = new Random();
         public WindowSpline()
         {
             InitializeComponent();
 
-            double[,] coord = new double[8, 2];
-            double[,] coordSpline = new double[80, 2];
+            double[,] coord = new double[NumOfPoints, 2];
+            double[,] coordSpline = new double[NumOfPoints * 10, 2];
             int cnt = 0;
             int cnt2 = 0;
 
             
             Polygon myPolygon = new Polygon();
-            myPolygon.Stroke = Brushes.Orange; 
+            myPolygon.Stroke = Brushes.DarkBlue; 
             myPolygon.StrokeThickness = 2;
             myPolygon.HorizontalAlignment = HorizontalAlignment.Left;
             myPolygon.VerticalAlignment = VerticalAlignment.Center;
-            PointCollection myPointCollection = new PointCollection();
 
-            for (int i = 0; i < 8; i++)
-                myPointCollection.Add(new Point(random.Next(10, 700), random.Next(10, 400)));
+            // Заполнение полигонов, заранее определенное количество
+            for (int i = 0; i < NumOfPoints; i++)
+                myPolygon.Points.Add(new Point(random.Next(10, 700), random.Next(10, 400)));
 
-            myPolygon.Points = myPointCollection;
             paintSurface.Children.Add(myPolygon);
 
-            foreach (Point point in myPointCollection)
+            foreach (Point point in myPolygon.Points)
             {
                 coord[cnt, 0] = point.X;
                 coord[cnt, 1] = point.Y;
                 cnt++;
             }
 
-            for (int i = 1; i <= myPolygon.Points.Count; i++)//для каждой из 8 записанных точек
+            for (int i = 0; i < myPolygon.Points.Count; i++)//для каждой из 8 записанных точек
             {
                 double[,] temp = Spline(coord, i);//находим 10 точек сплайна. Функция описана ниже
                 /*Записываем результат в массив точек сплайна*/
@@ -64,12 +60,12 @@ namespace GraphEditor
                 }
             }
             Polyline mySplinePolygon = new Polyline();
-            mySplinePolygon.Stroke = Brushes.Red; 
+            mySplinePolygon.Stroke = Brushes.DarkCyan; 
             mySplinePolygon.StrokeThickness = 2;
             mySplinePolygon.HorizontalAlignment = HorizontalAlignment.Left;
             mySplinePolygon.VerticalAlignment = VerticalAlignment.Center;
             PointCollection mySplinePointCollection = new PointCollection();
-            for (int i = 0; i < 80; i++)
+            for (int i = 0; i < NumOfPoints * 10; i++)
             {
                 mySplinePointCollection.Add(new Point(coordSpline[i, 0], coordSpline[i, 1]));
             }
@@ -203,6 +199,7 @@ namespace GraphEditor
                 }
             }
         }
+
         private void paintSurface_MouseUp(object sender, MouseButtonEventArgs e)
         {
             if (list != null)
@@ -225,7 +222,7 @@ namespace GraphEditor
 
                 }
                 Polygon myPolygon = new Polygon();
-                myPolygon.Stroke = Brushes.Blue; //основные линии
+                myPolygon.Stroke = Brushes.DarkBlue; //основные линии
                 myPolygon.StrokeThickness = 2;
                 myPolygon.HorizontalAlignment = HorizontalAlignment.Left;
                 myPolygon.VerticalAlignment = VerticalAlignment.Center;
@@ -246,8 +243,8 @@ namespace GraphEditor
                     }
 
                 }
-                double[,] coord = new double[8, 2];
-                double[,] coordSpline = new double[80, 2];
+                double[,] coord = new double[NumOfPoints, 2];
+                double[,] coordSpline = new double[NumOfPoints * 10, 2];
                 int cnt = 0;
                 int cnt2 = 0;
                 foreach (Point point in list)
@@ -256,7 +253,7 @@ namespace GraphEditor
                     coord[cnt, 1] = point.Y;
                     cnt++;
                 }
-                for (int i = 1; i <= 8; i++)//для каждой из 8 записанных точек
+                for (int i = 0; i < NumOfPoints; i++)//для каждой из 8 записанных точек
                 {
                     double[,] temp = Spline(coord, i);//находим 10 точек сплайна. Функция описана ниже
                     /*Записываем результат в массив точек сплайна*/
@@ -268,12 +265,12 @@ namespace GraphEditor
                     }
                 }
                 Polyline mySplinePolygon = new Polyline();
-                mySplinePolygon.Stroke = Brushes.Green;
+                mySplinePolygon.Stroke = Brushes.DarkCyan;
                 mySplinePolygon.StrokeThickness = 2;
                 mySplinePolygon.HorizontalAlignment = HorizontalAlignment.Left;
                 mySplinePolygon.VerticalAlignment = VerticalAlignment.Center;
                 PointCollection mySplinePointCollection = new PointCollection();
-                for (int i = 0; i < 80; i++)
+                for (int i = 0; i < NumOfPoints * 10; i++)
                 {
                     mySplinePointCollection.Add(new Point(coordSpline[i, 0], coordSpline[i, 1]));
                 }
@@ -283,47 +280,65 @@ namespace GraphEditor
                 list = null;
             }
         }
-        public double[,] Spline(double[,] coord, int first)//функция сплайн, по входящим точкам, и по номеру текущей точки, находит координаты точек сплайна
+        /// <summary>
+        /// Вычсляет Сплайн, на входе все точки и индекс текущей точки
+        /// </summary>
+        /// <param name="data">Матрица с координатами точек</param>
+        /// <param name="curr">Индекс точки, для которой ищется сплайн</param>
+        /// <returns>Матрица 10 на 2 с координатами точек сплайна</returns>
+        public double[,] Spline(double[,] data, int curr)//функция сплайн, по входящим точкам, и по номеру текущей точки, находит координаты точек сплайна
         {
-            int last = first - 1;//номер n-1 точки
-            int next = first + 1;//номер n+1 точки
-            int nextNext = first + 2;//номер n+2 точки
-            if (first == 1)//если номер точки 1, то предыдущая точка номер 8
+            int prev = curr - 1;//номер n-1 точки
+            int next = curr + 1;//номер n+1 точки
+            int nextNext = curr + 2;//номер n+2 точки
+            if (curr == 0)//если номер точки 1, то предыдущая точка номер 8
             {
-                last = 8;
+                prev = NumOfPoints - 1;
             }
-            if (first == 8)//если номер точки 8, то следующая точка номер 1, а n+2 номер 2
+            if (curr == NumOfPoints - 1)//если номер точки 8, то следующая точка номер 1, а n+2 номер 2
             {
-                next = 1;
-                nextNext = 2;
-            }
-            if (first == 7)//если номер точки 7,то n+2 номер 1
-            {
+                next = 0;
                 nextNext = 1;
             }
-            double[,] res = new double[10, 2];//массив результатов
-            double a0 = (coord[last - 1, 0] + (4 * coord[first - 1, 0]) + coord[next - 1, 0]) / 6;//формула коэфициента а0
-            double a1 = (-coord[last - 1, 0] + coord[next - 1, 0]) / 2;//формула коэфициента а1
-            double a2 = (coord[last - 1, 0] - (2 * coord[first - 1, 0]) + coord[next - 1, 0]) / 2;//формула коэфициента а2
-            double a3 = (-coord[last - 1, 0] + (3 * coord[first - 1, 0]) - (3 * coord[next - 1, 0]) + coord[nextNext - 1, 0]) / 6;//формула коэфициента а3
-            double b0 = (coord[last - 1, 1] + (4 * coord[first - 1, 1]) + coord[next - 1, 1]) / 6;//формула коэфициента b0
-            double b1 = (-coord[last - 1, 1] + coord[next - 1, 1]) / 2;//формула коэфициента b1
-            double b2 = (coord[last - 1, 1] - (2 * coord[first - 1, 1]) + coord[next - 1, 1]) / 2;//формула коэфициента b2
-            double b3 = (-coord[last - 1, 1] + (3 * coord[first - 1, 1]) - (3 * coord[next - 1, 1]) + coord[nextNext - 1, 1]) / 6;//формула коэфициента b3
-            double t = 0;//единичный шаг
-            /*подставляем полученные коэфициенты в уравнение*/
-            for (int i = 0; i < 10; i++)
+            if (curr == NumOfPoints - 2)//если номер точки 7,то n+2 номер 1
             {
-                res[i, 0] = a0 + (a1 * t) + (a2 * t * t) + (a3 * t * t * t);//полученная координата х
-                res[i, 1] = b0 + (b1 * t) + (b2 * t * t) + (b3 * t * t * t);//полученная координата у
-                t += 0.1;//увеличиваем шаг
+                nextNext = 0;
             }
-            return res;//возвращаем массив результатов
+
+            double[,] output = new double[10, 2];
+            // Создание и заполнение массива с конечными коэфициентами
+            double[] coorX = {
+                a0(data[prev, 0], data[curr, 0], data[next, 0]),
+                a1(data[prev, 0], data[next, 0]),
+                a2(data[prev, 0], data[curr, 0], data[next, 0]),
+                a3(data[prev, 0], data[curr, 0], data[next, 0], data[nextNext, 0])
+            };
+            double[] coorY = {
+                a0(data[prev, 1], data[curr, 1], data[next, 1]),
+                a1(data[prev, 1], data[next, 1]),
+                a2(data[prev, 1], data[curr, 1], data[next, 1]),
+                a3(data[prev, 1], data[curr, 1], data[next, 1], data[nextNext, 1]) 
+            };
+
+            double t = 0;//единичный шаг
+            
+            for (int i = 0; i < 10; i++, t += 0.1)
+            {
+                //подставляем полученные коэфициенты в уравнение
+                output[i, 0] = coorX[0] + coorX[1] * t + coorX[2] * t * t + coorX[3] * t * t * t; //полученная координата х
+                output[i, 1] = coorY[0] + coorY[1] * t + coorY[2] * t * t + coorY[3] * t * t * t; //полученная координата у
+            }
+
+            return output; // Возвращаем массив с конечными координатами
         }
+
+        #region формулы сплайна
         private double a0(double prev, double curr, double next) { return (prev + (4 * curr) + next) / 6; }
         private double a1(double prev, double next) { return (-prev + next) / 2; }
         private double a2(double prev, double curr, double next) { return (prev - (2 * curr) + next) / 2; }
         private double a3(double prev, double curr, double next, double doubNext) { return (-prev + (3 * curr - (3 * next) + doubNext)) / 6; }
+        #endregion
+
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.IsRepeat) { return; }
